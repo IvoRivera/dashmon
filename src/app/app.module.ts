6d0a31eb0b88126239891019c12b3d4b97a5079a
Ivo Rivera <ivorivera94@gmail.com>
2022-11-16 02:05:11 -0300

    utilizacion de plantilla, pendientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,72 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-// Modulos
 import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+import { PagesModule } from './pages/pages.module';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule} from '@angular/fire/compat/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import {HttpClientModule} from '@angular/common/http';
-import { NgApexchartsModule } from "ng-apexcharts";
-
-
-// Componentes
-import { AppComponent } from './app.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { RegistroComponent } from './components/registro/registro.component';
-import { LoginComponent } from './components/login/login.component';
-import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
-import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { InventarioComponent } from './components/inventario/inventario.component';
-import { CreateItemComponent } from './components/create-item/create-item.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { SpinnerComponent } from './shared/spinner/spinner.component';
-
-// Extra
-import { environment } from 'src/environments/environment';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent,
-    LoginComponent,
-    RegistroComponent,
-    DashboardComponent,
-    InventarioComponent,
-    CreateItemComponent,
-    SidenavComponent,
-    SpinnerComponent,
-    RecuperarPasswordComponent,
-    VerificarCorreoComponent,
+    NopageFoundComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatCardModule,
-    MatInputModule,
+    AuthModule,
+    PagesModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule,
-    NgApexchartsModule,
-    ToastrModule.forRoot(),
-    BrowserAnimationsModule,    
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
+    AngularFireAuthModule,
     provideFirestore(() => getFirestore()),
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
 
   ],
   providers: [],
