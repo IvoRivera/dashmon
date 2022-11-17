83384ba18cd682fb26e2fc0fc05919962b36d564
Ivo Rivera <ivorivera94@gmail.com>
2022-11-16 23:38:21 -0300

    modificacion de iconos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 
@@ -12,18 +10,14 @@ import { RouterModule } from '@angular/router';
   declarations: [
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent,
-    FooterComponent
   ],
   imports: [
     CommonModule,
     RouterModule
-  ], 
+  ],
   exports:[
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent,
-    FooterComponent
   ]
 })
 export class SharedModule { }
